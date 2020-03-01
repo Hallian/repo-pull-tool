01abc6f3098ce36509892f66593c97e42437447f
Nikolas Lahtinen <nikolas.lahtinen@gmail.com>
2020-03-01 10:24:57 +0200

    Add gh user and list only repos params to argbash

--- lib/argbash.sh
+++ lib/argbash.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # ARG_OPTIONAL_SINGLE([filter],[f],[regex filters, space separated list])
-# ARG_OPTIONAL_SINGLE([github-org],[g],[GitHub Organisation name])
+# ARG_OPTIONAL_SINGLE([github-org],[o],[GitHub Organisation name])
+# ARG_OPTIONAL_SINGLE([github-user],[u],[GitHub User name])
+# ARG_OPTIONAL_SINGLE([list-only],[l],[List repos only])
 # ARG_POSITIONAL_SINGLE([workspace],[directory where git repositories are located],[])
 # ARG_HELP([Clone or pull multiple GitHub organization repositories.])
 # ARGBASH_GO()
